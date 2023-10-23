7da5647daf2fead05c8c890cb33efeefd4cd548b
Devvaru <syeraru@gmail.com>
2023-10-23 21:19:41 +0200

    Test player creation

--- main.rb
+++ main.rb
@@ -2,3 +2,9 @@ require 'pp'
 require './players'
 require './math_questions'
 require './matches'
+
+# player1 = Player.new("Dave")
+# puts "#{player1.name} has #{player1.lives} lives left"
+
+player2 = Player.new
+puts "#{player2.name} has #{player2.lives} lives left"
--- players.rb
+++ players.rb
@@ -1,7 +1,7 @@
 class Player
     attr_accessor :name, :lives
 
-    def initialize(name, lives = 3)
+    def initialize(name = gets.chomp, lives = 3)
         @name = name
         @lives = lives
     end
